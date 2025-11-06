694e049cf9127a03223882a0eece09ad69e19c15
Saaket Baldawa <99862185+saaket2006@users.noreply.github.com>
2025-11-06 22:13:41 +0530

    Refactor LLM initialization in summarizer.py

--- Project/crew/summarizer.py
+++ Project/crew/summarizer.py
@@ -1,12 +1,15 @@
 from crewai import Agent
+from crewai.llm import LLM
+
+llm = LLM(
+    provider="groq",
+    model="llama-3.1-8b-instant"
+)
 
 summarizer = Agent(
     role="Summarizer",
     goal="Convert cleaned text into structured, meaningful summaries.",
     backstory="Skilled at extracting key insights.",
-    llm={
-        "provider": "groq",
-        "model": "llama-3.1-8b-instant"
-    },
+    llm=llm,
     verbose=True
 )
