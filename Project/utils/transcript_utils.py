417318c7afb1a7e34a94bde229710dcc64255ae0
Saaket Baldawa <99862185+saaket2006@users.noreply.github.com>
2025-11-06 22:42:59 +0530

    Update transcript_utils.py

--- Project/utils/transcript_utils.py
+++ Project/utils/transcript_utils.py
@@ -1,4 +1,4 @@
-from youtube_transcript_api import YouTubeTranscriptApi, TranscriptDisabled
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 
 def extract_video_id(url):
     if "v=" in url:
@@ -11,5 +11,5 @@ def load_youtube_transcript(video_id):
     try:
         transcript_data = YouTubeTranscriptApi.get_transcript(video_id)
         return " ".join([x['text'] for x in transcript_data])
-    except TranscriptDisabled:
+    except TranscriptsDisabled:
         return None
