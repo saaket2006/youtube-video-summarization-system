bb08a54fbc8a1bb6d5b8ccfbc263e03bfc8333e2
Saaket Baldawa <99862185+saaket2006@users.noreply.github.com>
2025-11-06 22:13:19 +0530

    Refactor LLM initialization in query_agent.py

--- Project/crew/query_agent.py
+++ Project/crew/query_agent.py
@@ -1,12 +1,15 @@
 from crewai import Agent
+from crewai.llm import LLM
+
+llm = LLM(
+    provider="groq",
+    model="llama-3.1-8b-instant"
+)
 
 query_agent = Agent(
     role="Q&A Assistant",
     goal="Answer user questions based on final summary only.",
     backstory="Helpful and accurate.",
-    llm={
-        "provider": "groq",
-        "model": "llama-3.1-8b-instant"
-    },
+    llm=llm,
     verbose=True
 )
