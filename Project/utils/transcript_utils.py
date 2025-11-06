e28b4ab1c65c6f50f166ba3d835154e463f97f08
Saaket Baldawa <99862185+saaket2006@users.noreply.github.com>
2025-11-06 21:21:26 +0530

    Add transcript utility functions for YouTube videos

---
+++ Project/utils/transcript_utils.py
@@ -0,0 +1,15 @@
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptDisabled
+
+def extract_video_id(url):
+    if "v=" in url:
+        return url.split("v=")[-1].split("&")[0]
+    elif "youtu.be/" in url:
+        return url.split("youtu.be/")[-1].split("?")[0]
+    return None
+
+def load_youtube_transcript(video_id):
+    try:
+        transcript_data = YouTubeTranscriptApi.get_transcript(video_id)
+        return " ".join([x['text'] for x in transcript_data])
+    except TranscriptDisabled:
+        return None
